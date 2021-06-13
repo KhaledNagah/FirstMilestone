07edbae02dc628fbac31db37caba1496e8442ebf
BaSsioonii <63972053+BaSsioonii@users.noreply.github.com>
2021-06-13 15:47:55 +0200

    Adding the longitude parsing function

--- parse.c
+++ parse.c
@@ -37,3 +37,42 @@ float parse_lat(char* GpsData)
 	Lat = atof(Latitude);
 	return Lat;
 }
+float parse_long(char* GpsData)
+{
+	int length=70;
+	int i = 0;
+	int Count = 0;
+	char Longitude[9];
+	float Longt;
+	int j = 0, k = 0;
+	int start= 0;
+	int  End = 0;
+	int temp = 0;
+	int flag = 0;
+		for (i = 0; i < length; i++)
+		{
+
+			if (GpsData[i] == ',')
+			{
+				Count++;
+			}
+			if (Count == 4 && temp == 0)
+			{
+				start = i + 1;
+				temp = 1;
+			}
+			if (Count == 5 && flag == 0)
+			{
+
+				End = i - 1;
+				flag = 1;
+				break;
+			}
+		}
+		for (j = start, k = 0; j <= End; j++, k++)
+		{
+			Longitude[k] = GpsData[j];
+		}
+		Longt = atof(Longitude);
+	return Longt;
+}
